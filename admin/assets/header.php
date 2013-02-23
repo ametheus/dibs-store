0b8d1431f4fbf3641ebacba5f26e2b7ab9351cb8
Thijs van Dijk <github@inurbanus.nl>
2013-02-23 20:37:34 +0100

    Navigation bar

--- admin/assets/header.php
+++ admin/assets/header.php
@@ -2,6 +2,22 @@
 
 header( "Content-type: text/html; charset=UTF-8" );
 
+$link = function( $href )
+{
+	global $api_root;
+	
+	list($script_name) = explode( "?", $_SERVER["REQUEST_URI"] );
+	$href = $api_root . "admin/" . $href;
+	
+	$rv = "<a href=\"" . htmlspecialchars($href) . "\">";
+	
+	list($scr) = explode( "?", $href );
+	if ( $scr == $script_name )
+		return "<li class=\"active\">" . $rv;
+	
+	return "<li>" . $rv;
+};
+
 ?><!DOCTYPE html>
 <html>
 	<head>
@@ -16,5 +32,23 @@ header( "Content-type: text/html; charset=UTF-8" );
 		
 	</head>
 	<body>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+					</button>
+					<a class="brand" href="<?=$api_root?>admin/">Dibs admin</a>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<?=$link( "orders.php" )?>Orders</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>
+		</div>
 		<div class="canvas">
 			
+<?php
+
+unset($link);
