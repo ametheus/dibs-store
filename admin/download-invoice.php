57a65b79de48988c1d72761c3503b4755aa1dc01
Thijs van Dijk <github@inurbanus.nl>
2013-02-23 16:36:17 +0100

    Download the invoice rather than the freight letter, if requested

--- admin/download-invoice.php
+++ admin/download-invoice.php
@@ -5,9 +5,13 @@ require_once( "lib/invoice.inc" );
 
 $cart = Cart::get( (string)$_REQUEST["cart-id"], $short = 0 );
 
-$f = Invoice::generate_invoice( $cart, $billing_address = false, $copy = false );
+
+$billing_address = ! empty($_REQUEST["billing"]);
+$f = Invoice::generate_invoice( $cart, $billing_address, $copy = false );
+
 
 header( "Content-disposition: attachment; filename=invoice-{$cart["invoice-no"]}.pdf" );
 header( "Content-type: application/pdf" );
 
 readfile( $f );
+unlink( $f );
